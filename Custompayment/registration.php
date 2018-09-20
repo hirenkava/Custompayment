ecdd7625e5e98ee859f5ca88c45b07915e9216ea
Hiren Kava <hiren.kava84@gmail.com>
2018-09-21 00:43:47 +0530

    add billing and shipping custom field url

--- Custompayment/registration.php
+++ Custompayment/registration.php
@@ -2,6 +2,7 @@
 /**
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
+ https://codeblog.experius.nl/magento-2-add-extra-billing-or-shipping-field-to-the-checkout/
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
